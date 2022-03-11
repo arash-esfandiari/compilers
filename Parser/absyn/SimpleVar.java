e30ad4ae8e73e11993bedeef7d109e263e920a86
Arash Esfandiari <arashesfandiari@yahoo.com>
2022-03-10 19:04:41 -0500

    s

--- Parser/absyn/SimpleVar.java
+++ Parser/absyn/SimpleVar.java
@@ -1,13 +1,13 @@
 package absyn;
 
 public class SimpleVar extends Var {
-    
+
     public String name;
 
     public SimpleVar(int row, int col, String name) {
         this.row = row;
         this.col = col;
-        this.name = name
+        this.name = name;
     }
 
     public void accept(AbsynVisitor visitor, int level) {
